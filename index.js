05fb470ae7af5e7cab6fe76016eccb5d684f05bb
Ben Christiansen <bwc1@iastate.edu>
2024-02-29 22:45:39 -0600

    added javascript functionality

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+fetch('./data.json')
+    .then(response => response.json())
+    .then(myMovies => loadMovies(myMovies));
 
+function loadMovies(myMovies) {
+    var mainContainer = document.getElementById("goodmovies");
+    for (var i = 0; i < myMovies.movies.length; i++) {
+        let title = myMovies.movies[i].title;
+        let year = myMovies.movies[i].year;
+        let url = myMovies.movies[i].url;
+        let rating = myMovies.movies[i].rating;
+
+        let div = document.createElement("div");
+        div.innerHTML = `<h3>${title}</h3> ${year} <br> ${rating} <br> <img src=${url} width = "200"> <br> <br>`;
+        mainContainer.appendChild(div);
+        console.log(div)
+    }
+}
